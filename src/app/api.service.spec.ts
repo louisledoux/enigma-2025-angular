7ee36f0a816806085210af520d683507936de298
Louis Ledoux <19322697+louisledoux@users.noreply.github.com>
2025-12-18 12:30:53 +0100

    test(api): add tests

--- src/app/api.service.spec.ts
+++ src/app/api.service.spec.ts
@@ -1,16 +1,36 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { mockedProduct } from './data/mockedProduct';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(ApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch products', () => {
+    const mockedProducts = [mockedProduct];
+
+    service.fetchProducts().subscribe((products) => {
+      expect(products).toEqual(mockedProducts);
+      expect(service.products()).toEqual(mockedProducts);
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:3000/products');
+    expect(req.request.method).toBe('GET');
+    req.flush({ data: mockedProducts });
   });
 });
